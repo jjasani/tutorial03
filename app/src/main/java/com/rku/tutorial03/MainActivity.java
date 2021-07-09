ee51bed4f07a7ef3a1f006961838f69b679eed10
Pinaka Code Studio <62845743+pinakacodestudio@users.noreply.github.com>
2021-07-09 10:28:25 +0530

    Initial Commit

---
+++ app/src/main/java/com/rku/tutorial03/MainActivity.java
@@ -0,0 +1,23 @@
+package com.rku.tutorial03;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void loginClick(View view) {
+        Toast.makeText(this, "Login Clicked.", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MainActivity.this,Welcome.class);
+        startActivity(intent);
+        finish();
+    }
+}
